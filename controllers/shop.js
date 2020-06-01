acf2cde4ae1f392d9dc8d6b4de07d9a74cf5240f
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-05-31 20:22:13 -0400

    add the getCart method

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,11 +4,8 @@ exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'All Products',
+      path: '/products'
     });
   });
 };
@@ -18,10 +15,14 @@ exports.getIndex = (req, res, next) => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      path: '/'
     });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    pageTitle: 'Your Cart',
+    path: '/cart'
   });
 };
