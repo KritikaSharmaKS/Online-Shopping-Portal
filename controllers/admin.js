0bede47438e957736806143bb1a1cc5c8eb5dd56
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-06-27 20:32:29 -0400

    add method to post delete request

--- controllers/admin.js
+++ controllers/admin.js
@@ -58,3 +58,8 @@ exports.getAddProduct = (req, res, next) => {
     });
   };
   
+  exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
+  };
