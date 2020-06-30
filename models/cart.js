1b01844bfe4454a10fffd30955bcea5ef64602fa
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-06-29 20:44:52 -0400

    fix bug

--- models/cart.js
+++ models/cart.js
@@ -44,6 +44,9 @@ const p = path.join(
             }
             const updatedCart = { ...JSON.parse(fileContent) };
             const product = updatedCart.products.find(prod => prod.id === id);
+            if(!product){
+                return;
+            }
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
             updatedCart.totalPrice = updatedCart.totalPrice -  product.qty * productPrice;
             fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
