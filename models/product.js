25c3f26871f15bd3ebdc278e859fb3d28e23222a
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-06-29 21:06:52 -0400

    fix bug with delete product

--- models/product.js
+++ models/product.js
@@ -20,7 +20,7 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(id, title, imageUrl, description, price ) {
+  constructor(id, title, imageUrl, description, price) {
     this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
@@ -30,33 +30,35 @@ module.exports = class Product {
 
   save() {
     getProductsFromFile(products => {
-      if(this.id){
-        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
         const updatedProducts = [...products];
         updatedProducts[existingProductIndex] = this;
         fs.writeFile(p, JSON.stringify(updatedProducts), err => {
           console.log(err);
         });
-      }else{
+      } else {
         this.id = Math.random().toString();
         products.push(this);
         fs.writeFile(p, JSON.stringify(products), err => {
           console.log(err);
-        }); 
+        });
       }
     });
   }
 
   static deleteById(id) {
     getProductsFromFile(products => {
-      const product = products.filter(prod => prod.id === id);
-      const updatedProducts = products.findIndex(prod => prod.id !== this.id);
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if(!err){
+        if (!err) {
           Cart.deleteProduct(id, product.price);
         }
       });
-    });  
+    });
   }
 
   static fetchAll(cb) {
