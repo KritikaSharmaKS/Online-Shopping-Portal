b700f09759a4c56e6feab3f2444859c4e57d3721
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-07-12 09:08:57 -0400

    insert data into db using the save() method

--- models/product.js
+++ models/product.js
@@ -12,11 +12,12 @@ module.exports = class Product {
   }
 
   save() {
-
+    db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ? ?)', 
+    [this.title, this.price, this.imageUrl, this.description]);
   }
 
   static deleteById(id) {
-   
+
   }
 
   static fetchAll() {
