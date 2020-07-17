b17371136727eb071eb91366dc10492dbeeba5b1
Kritika Sharma <kritika.sharma@sinopetech.com>
2020-07-17 16:29:14 -0400

    sync js model definitions to db

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
+sequelize.execute('SELECT * FROM products')
     .then((result) => {
         console.log(result[0], result[1]);
     })
@@ -30,4 +30,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+    .sync() //create tables in db based on your models
+    .then(result => {
+        console.log(result);
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    }); 
+
+
