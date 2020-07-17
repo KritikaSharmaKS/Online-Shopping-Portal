e5eabb10be19980ee0256b12103e8c88caa36b4b
Kritika Sharma <kritika.sharma@sinopetech.com>
2020-07-17 16:21:19 -0400

    complete defining the product model

--- models/product.js
+++ models/product.js
@@ -1,2 +1,28 @@
 const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');
+const { Model } = require('sequelize');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING, 
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+});
+
+module.exports = Product;
