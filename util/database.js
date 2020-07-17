20f3f7934cd9b289720803d9e6bf635c885d122c
Kritika Sharma <kritika.sharma@sinopetech.com>
2020-07-17 16:13:45 -0400

    establish db connection using sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    database : 'node-complete',
-    password : '9871709924'
+// const pool = mysql.createPool({
+//     host : 'localhost',
+//     user : 'root',
+//     database : 'node-complete',
+//     password : '9871709924'
+// });
+
+// module.exports = pool.promise();
+
+//------------------------------------------------------
+// Use Sequelize
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', '9871709924', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
