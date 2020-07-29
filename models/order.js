be9bc0ff6e71ad2e17b5414436285ea933c7cdf2
Kritika Sharma <kritika.sharma@sinopetech.com>
2020-07-29 16:52:49 -0400

    rectify variable name

--- models/order.js
+++ models/order.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const CartItem = sequelize.define('order', {
+const Order = sequelize.define('order', {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
