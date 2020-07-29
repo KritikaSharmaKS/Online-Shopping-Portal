5f4415a8878fdba055504e467fa53596cfbeee54
Kritika Sharma <kritika.sharma@sinopetech.com>
2020-07-29 15:14:36 -0400

    manage the dummy user

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findById(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
