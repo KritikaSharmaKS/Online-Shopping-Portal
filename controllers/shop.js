9e361a51fb47819e4c262907a46f125bd4df02af
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-07-30 21:33:06 -0400

    add postOrder method to store cart items as order items

--- controllers/shop.js
+++ controllers/shop.js
@@ -123,7 +123,23 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
- 
+ req.user.getCart()
+  .then(cart => {
+    return cart.getProducts();
+  })
+  .then(products => {
+    req.user
+      .createOrder()
+      .then(order => {
+        order.addProducts(products.map(product => {
+          product.orderItem = { quantity: product.cartItem.quantity };
+          return product;
+        }));
+      })
+      .catch(err => console.log(err));
+  })
+  .then(result => res.redirect('/orders'))
+  .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
