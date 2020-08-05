227bf5383b0500ee13e42cd171adcc84d4cdcb60
Kritika Sharma <kritikasharma462@gmail.com>
2020-08-05 16:25:22 -0400

    use the fetchAll method to retrieve all the products

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,9 +70,7 @@ exports.postAddProduct = (req, res, next) => {
 // };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-  .getProducts()
-  //Product.findAll()
+  Product.fetchAll()
   .then(products => {
     res.render('admin/products', {
       prods: products,
