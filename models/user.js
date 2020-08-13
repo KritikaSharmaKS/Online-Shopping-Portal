046cb5753b5ea4cfe3b8adb430f560552b574a1c
Kritika Sharma <kritikasharma462@gmail.com>
2020-08-13 13:26:28 -0400

    re-write the user model using mongodb

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save(){
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .find({ _id: new mongodb.ObjectId(userId) })
+      .next()
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  
+}
+
+module.exports = User;
