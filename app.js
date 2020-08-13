d0a1658b56bb62c63283ec483b44612c5953235b
Kritika Sharma <kritikasharma462@gmail.com>
2020-08-13 13:35:35 -0400

    create a user behind the scenes for testing

--- app.js
+++ app.js
@@ -13,9 +13,20 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+    next();    
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
