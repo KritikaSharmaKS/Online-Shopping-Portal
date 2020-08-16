bd83d69b2a4f2dc6d29e9ff2290991fdc244a813
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-08-16 18:18:49 -0400

    add addToCart() user method

--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email, cart) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
     this.cart = cart; //{items: []}
+    this.id = id;
   }
 
   save(){
@@ -13,8 +14,17 @@ class User {
     return db.collection('users').insertOne(this);
   }
 
-  addToCart() {
-
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // }); 
+    const updatedCart = {items: [{ ...product, quantity:1 }]};
+    const db = getDb();
+    return db
+      .collection('users').updateOne(
+        { _id: new mongodb.ObjectID(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
