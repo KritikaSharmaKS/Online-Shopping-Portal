f30a8e1f3e8491d05fe062df8c0085172eb39757
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-08-16 18:53:35 -0400

    create new user initially

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('5f3978644a538e59210f30d0')
       .then(user => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
       })
       .catch(err => console.log(err));
