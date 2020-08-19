33d4efd377b346c25c38743d2dd9ecf2019087f8
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-08-18 20:33:05 -0400

    complete the addToCart method to user model

--- models/user.js
+++ models/user.js
@@ -15,14 +15,29 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex(cp => {
-    //   return cp._id === product._id;
-    // }); 
-    const updatedCart = {items: [{ ...product, quantity:1 }]};
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
     const db = getDb();
     return db
-      .collection('users').updateOne(
-        { _id: new mongodb.ObjectID(this._id) },
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } }
       );
   }
