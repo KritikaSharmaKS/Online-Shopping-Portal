96b286bf595e50b340a620e45f409713a43ddc18
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-09-12 12:45:40 -0400

    fix an old bug related to product details page

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,24 +15,24 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.fetchAll({ where: { id: prodId } })
-    .then((products) => {
+  // Product.fetchAll({ where: { id: prodId } })
+  //   .then((products) => {
+  //     res.render("shop/product-detail", {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => console.log(err));
+  Product.findById(prodId)
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: products[0],
-        pageTitle: products[0].title,
+        product: product,
+        pageTitle: product.title,
         path: "/products",
       });
     })
     .catch((err) => console.log(err));
-  // Product.findByPk(prodId)
-  // .then(product => {
-  //   res.render( 'shop/product-detail', {
-  //     product: product,
-  //     pageTitle: product.title,
-  //     path: '/products'
-  //   });
-  // })
-  // .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
