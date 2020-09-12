18ff18629fa0098e6b51a445882cc733e75c7e16
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-09-12 13:53:13 -0400

    deleting cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -68,55 +68,19 @@ exports.postCart = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
-      res.redirect('/cart');
+      res.redirect("/cart");
     });
-  //   let fetchedCart;
-  //   let newQuantity = 1;
-  //   req.user
-  //     .getCart()
-  //     .then(cart => {
-  //       fetchedCart = cart;
-  //       return cart.getProducts({ where: { id: prodId } });
-  //     })
-  //     .then(products => {
-  //       let product;
-  //       if (products.length > 0) {
-  //         product = products[0];
-  //       }
-  //       if (product) {
-  //         //if there is an existing product => retrieve it and increment the quantity
-  //         const oldQuantity = product.cartItem.quantity;
-  //         newQuantity = oldQuantity + 1;
-  //         return product;
-  //       }
-  //       //this is a new product
-  //       return Product.findByPk(prodId)
-  //     })
-  //     .then(product => {
-  //       return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
-  //     })
-  //     .then(() => {
-  //       res.redirect('/cart');
-  //     })
-  //     .catch(err => console.log(err));
 };
 
-// exports.postCartDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   req.user
-//     .getCart()
-//     .then(cart => {
-//       return cart.getProducts({ where: { id: prodId } });
-//     })
-//     .then(products => {
-//       const product = products[0];
-//       return product.cartItem.destroy()
-//     })
-//     .then(result => {
-//       res.redirect('/cart');
-//     })
-//     .catch(err => console.log(err));
-// };
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .deleteItemFromCart(prodId)
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
 
 // exports.getOrders = (req, res, next) => {
 //   req.user
