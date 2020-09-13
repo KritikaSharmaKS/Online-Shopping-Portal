ab8a83e88fb6f53ecea1428cf5cf4dc900176fb1
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-09-12 22:02:13 -0400

    fetching all products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 // const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
@@ -36,7 +36,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("shop/index", {
         prods: products,
