6ec2c0adb3523d2e84021b28b5581d34f43ae65d
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-09-12 22:23:45 -0400

    deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -86,7 +86,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.delete(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
