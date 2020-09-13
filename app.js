41fc994e7c5a418ba3dccac20b3c4ff751662e7e
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-09-12 21:06:53 -0400

    connecting to mongodb server with mongoose

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const errorController = require("./controllers/error");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const User = require('./models/user');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const User = require("./models/user");
+const e = require("express");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('5f5977ef5f45cfd9a4f535b1')
-    .then(user => {
+  User.findById("5f5977ef5f45cfd9a4f535b1")
+    .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://kritikasharma:QazFBmIiD3swPzhc@online-shopping-portal.h6buw.mongodb.net/shopDB?retryWrites=true&w=majority"
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
