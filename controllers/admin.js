bc6068e9947db5feab413561cb8c0f163db88efc
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-11-02 19:47:55 -0500

    adding authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -82,7 +82,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({userId: req.user._id})
     .then((products) => {
       res.render("admin/products", {
         prods: products,
