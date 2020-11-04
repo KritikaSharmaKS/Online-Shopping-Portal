8326e787700bbb0c62b4d3a2f76e81a8f225cdb4
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-11-03 21:59:20 -0500

    Built-in and custom validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check } = require("express-validator");
 
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
@@ -10,14 +10,25 @@ router.post("/login", authController.postLogin);
 
 router.post("/logout", authController.postLogout);
 
-router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
+router.get("/signup", authController.getSignup);
+router.post(
+  "/signup",
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email address is forbidden");
+      }
+      return true;
+    }),
+  authController.postSignup
+);
 
 router.get("/reset", authController.getReset);
 router.post("/reset", authController.postReset);
 
 router.get("/reset/:token", authController.getNewPassword);
 router.post("/new-password", authController.postNewPassword);
-
 
 module.exports = router;
