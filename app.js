a427977f3cdda1b004877aea6a06a627cc98527c
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-02-20 23:26:08 -0500

    Handling File Uploads with Multer

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
+const multer = require("multer");
 
 const User = require("./models/user");
 const errorController = require("./controllers/error");
@@ -30,6 +31,7 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({dest: 'images'}).single('image'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
