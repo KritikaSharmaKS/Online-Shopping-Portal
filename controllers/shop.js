e5364cadf4ec5fd1674588c7f7fda1be918dc06a
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-03-13 22:20:03 -0500

    Re-Using the Pagination Logic & Controls

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,12 +7,27 @@ const PDFDocument = require("pdfkit");
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page = Number(req.query.page) || 1;
+  let totalProducts;
+
+  Product.countDocuments()
+    .then((numberOfProducts) => {
+      totalProducts = numberOfProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalProducts,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalProducts / ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
@@ -40,7 +55,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = Number(req.query.page);
+  const page = Number(req.query.page) || 1;
   let totalProducts;
 
   Product.countDocuments()
