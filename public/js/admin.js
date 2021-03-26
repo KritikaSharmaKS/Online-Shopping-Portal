859508a912a5e7924842a305f014426b07ec6a60
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-03-25 22:31:53 -0400

    Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,7 @@
 deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+    const productElement = btn.closest('article');
 
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
@@ -9,7 +10,11 @@ deleteProduct = (btn) => {
         }
     })
     .then(result => {
-        console.log(result);
+        return result.json();
+    })
+    .then(data => {
+        console.log(data);
+        productElement.remove();
     })
     .catch(err => {
         console.log(err);
